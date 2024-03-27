1d96dc129507710c1c1bedccacd3177562f59ac3
4pygmalion <4pygmalion@naver.com>
2024-03-27 23:57:55 +0900

    publication: figure 3작성중

--- core/io_ops.py
+++ core/io_ops.py
@@ -1,5 +1,6 @@
 import pickle
 from typing import Any
+import json
 
 
 def save_pickle(object, path: str) -> None:
@@ -12,3 +13,7 @@ def save_pickle(object, path: str) -> None:
 def load_pickle(path: str) -> Any:
     with open(path, "rb") as fh:
         return pickle.load(fh)
+
+def read_json(path:str) -> dict:
+    with open(path, "r") as fh:
+        return json.load(fh)
